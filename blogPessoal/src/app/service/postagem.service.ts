1e2489b034a82b734a572fe9bef2b9cd69f453fc
Ivy-Silva <ivyrebeca@gmail.com>
2020-04-16 21:48:37 -0300

    Criação do endPoint getAllPostagens

--- blogPessoal/src/app/service/postagem.service.ts
+++ blogPessoal/src/app/service/postagem.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostagemService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllPostagens(){
+    return this.http.get('http://31.220.57.14:8080/postagens')
+  }
+
 }
+
+// CRUD - Create, Read, Update, Delete
+  // End point: Declarar no meu service, todos os pontos de contato do meu service, utilizando todos os pontos de contato de uma API (CRUD)
+  // CREATE: get
+  // READ: post
+  // UPDATE: put
+  // DELETE: delete
